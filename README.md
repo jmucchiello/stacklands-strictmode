d698a462b2997c856a4ce3a67f8a1d8b66967c27
Joe Mucchiello <jmucchiello@gmail.com>
2023-08-27 02:42:00 -0400

    Finished

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Stacklands Mod Template
+# Strict Mode
 
-For information about using the template, see the Modding Wiki
+When enabled, no card transitions can be performed unless you have discovered the idea first. When installed, this mod is disabled. You have to go into the options screen and turn it on. The game will restart whenever you turn this option on or off.
+
+That's it. This mod is most difficult when you clear the save before starting a new game. You can't even make a stick until you find the idea for it. (It's in the Humble Beginnings card stack.)
---
+++ StrictModeMod.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+namespace StrictModeModNS
+{
+    public class StrictModeMod : Mod
+    {
+        public ConfigEntry<bool> IsStrict;
+        private void Awake()
+        {
+            IsStrict = Config.GetEntry<bool>("strictmodemod_config_enable", false,
+                new ConfigUI() { NameTerm = "strictmodemod_config_enable",
+                    TooltipTerm = "strictmodemod_config_enable_tooltip",
+                    RestartAfterChange = true 
+                });
+        }
+        public override void Ready()
+        {
+            if (IsStrict.Value)
+            {
+                foreach (Blueprint blueprint in WorldManager.instance.BlueprintPrefabs)
+                {
+                    blueprint.IsInvention = true;
+                }
+            }
+            Logger.Log($"Strict Mode is {(IsStrict.Value ? "Enabled" : "Disabled")}");
+        }
+    }
+}
